34520e859f84086ee1d6e675d3310f07928ff4f7
Kirill Fedtsov <kirill@Air-M1.local>
2023-11-08 12:40:40 +0300

    created wtform - registration and login but no html

--- forms.py
+++ forms.py
@@ -1,2 +1,25 @@
 from flask_wtf import FlaskForm
-from wtform import StringField, PasswordField
+from wtform import StringField, PasswordField, SubmitField, BooleanField
+from wtform.validators import DataRequired, Length, EqualTo, Email 
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = StringField('Password',
+                           validators=[DataRequired()])
+    password_confirm = StringField('Password confirmation',
+                                   validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('SIGN IN')   
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = StringField('Password',
+                           validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit_button = SubmitField('LOGIN')
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9efc17d27f6b28690111ebc0e4cf7c2e'
 
 posts = [
         {
@@ -26,6 +30,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Registration", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
